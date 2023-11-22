3cde40f8ace8f665248403fd9f655099c2d7bd4e
John1on <iancoion1@gmail.com>
2023-11-22 15:48:32 +0200

    The logic of the application, and small corrections to designs

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redNumberLabel: UILabel!
+    @IBOutlet var greenNumberLabel: UILabel!
+    @IBOutlet var blueNumberLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 30
+        
+        changeColor()
+        
     }
 
+    @IBAction func SliderAction(_ sender: UISlider) {
+        changeColor()
+        
+        switch sender {
+        case redSlider:
+            redNumberLabel.text = String(format: "%.2f", redSlider.value)
+        case greenSlider:
+            greenNumberLabel.text = String(format: "%.2f", greenSlider.value)
+        default:
+            blueNumberLabel.text = String(format: "%.2f", blueSlider.value)
+        }
 
+    }
+    
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green:CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
 }
 
